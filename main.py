77ab2acfa2225eaf2ce9abee3cab4168cf018c76
Noel Buehler <buehlern@student.ethz.ch>
2025-04-28 14:04:49 +0200

    Add PCA + KNN model and evaluation

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from utils import load_config, load_dataset, load_test_dataset, print_results, s
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-from sklearn.decomposition import PCA
+from sklearn.decomposition import KernelPCA, PCA
 from sklearn import linear_model
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn import preprocessing
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 # sklearn imports...
 # SVRs are not allowed in this project.
@@ -18,8 +19,9 @@ if __name__ == "__main__":
     config = load_config()
 
     # Load dataset: images (X) and corresponding minimum distance values (y)
-    X = np.load("/Users/noel/Documents/SML Project/project1/images_train.npy")
-    y = np.load("/Users/noel/Documents/SML Project/project1/labels_train.npy")
+    #X = np.load("/Users/noel/Documents/SML Project/project1/images_train.npy")
+    #y = np.load("/Users/noel/Documents/SML Project/project1/labels_train.npy")
+    X, y = load_dataset(config)
     print(f"[INFO]: Dataset loaded with {len(X)} samples.")
 
     images_test = load_test_dataset(config)
@@ -28,40 +30,58 @@ if __name__ == "__main__":
     # possible preprocessing steps ... training the model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     # 1. Normalization
-    scaler = preprocessing.StandardScaler().fit(X)
-
+    scaler = preprocessing.StandardScaler().fit(X_train)
+    scaler = preprocessing.MinMaxScaler().fit(X_train)
 
     X_train_s = scaler.transform(X_train)
     X_test_s = scaler.transform(X_test)
 
-    pca = PCA(n_components=50)
+    #pca = KernelPCA(n_components=300, kernel="rbf")
+    pca = PCA(n_components=100, whiten=True)
     pca.fit(X_train_s)
 
     X_train_pp = pca.transform(X_train_s)
     X_test_pp = pca.transform(X_test_s)
 
+    #X_train_pp = X_train_s
+    #X_test_pp = X_test_s
 
     # regression
 
     # define model
     #model = linear_model.Ridge(alpha=.5)
-    model = KNeighborsRegressor(n_neighbors=2, weights="distance")
+    #model = KNeighborsRegressor(n_neighbors=2, weights="distance", metric="manhattan")  
     #model = DecisionTreeRegressor()
-    '''model = MLPRegressor(
-        hidden_layer_sizes=(500, 500, 500),
+    model = MLPRegressor(
+        hidden_layer_sizes=(300, 300, 300),
+        alpha=0.1,
         activation="relu",
         solver="adam",
-        max_iter=10000,
+        max_iter=500,
         random_state=42,
         tol=1e-10,
-        verbose=True,
+        verbose=False,
         learning_rate="adaptive",
         learning_rate_init=0.000005,
-    )'''
-   
-    # 4. fit model
-    model.fit(X_train_pp, y_train)
+    )
 
+
+    # fine tuning loop
+    for i in range(300):
+        for t in tqdm(range(50), desc="Training iterations"):
+            model.partial_fit(X_train_pp, y_train)
+
+        y_train_pred = model.predict(X_train_pp)
+        y_test_pred = model.predict(X_test_pp)
+
+        print(f"========= Iteration {i} =========")
+        print("Train Score:")
+        print_results(y_train, y_train_pred)
+        print("Test Score:")
+        print_results(y_test, y_test_pred)
+    
+    #model.fit(X_train_pp, y_train)
+        
     # predict
     X_pred = model.predict(X_test_pp)
     
